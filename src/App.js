dca3af447d9c08d769a1d4800a7dd5e707d15422
ElaineLeandro <elainerleandro1@gmail.com>
2023-08-16 09:16:50 -0300

    Aula 03 componte

--- src/App.js
+++ src/App.js
@@ -5,62 +5,74 @@ import Time from './componentes/Time';
 
 function App() {
 
-  const times = [
+
+  const [times, setTimes]= useState([
     {
       nome: 'Programação',
-      corPrimaria: '#57C278',
-      corSecundaria: '#D9F7E9'
+      cor: '#D9F7E9'
     },
     {
       nome: 'Front-End',
-      corPrimaria: '#82CFFA',
-      corSecundaria: '#E8F8FF'
+      cor: '#E8F8FF'
     },
     {
       nome: 'Data Science',
-      corPrimaria: '#A6D157',
-      corSecundaria: '#F0F8E2'
+      cor: '#F0F8E2'
     },
     {
       nome: 'Devops',
-      corPrimaria: '#E06B69',
-      corSecundaria: '#FDE7E8'
+      cor: '#FDE7E8'
     },
     {
       nome: 'UX e Design',
-      corPrimaria: '#DB6EBF',
-      corSecundaria: '#FAE9F5'
+      cor: '#FAE9F5'
     },
     {
       nome: 'Mobile',
-      corPrimaria: '#FFBA05',
-      corSecundaria: '#FFF5D9'
+      cor: '#FFF5D9'
     },
     {
       nome: 'Inovação e Gestão',
-      corPrimaria: '#FF8A29',
-      corSecundaria: '#FFEEDF'
+      cor: '#FFEEDF'
     }
-  ]
+  ]) 
 
   const [colaboradores, setColaboradores] = useState([])
 
+
   const aoNovoColaboradorAdicionado = (colaborador) => {
-    debugger
+    
     setColaboradores([...colaboradores, colaborador])
+  }
+
+  function deletarColaborador() {
+    console.log("Caiu aqui")
+  }
+
+  function mudarCorDoTime(cor, nome){
+    setTimes(times.map( time => {
+      if(time.nome === nome){
+        time.cor = cor
+      }
+      return time
+    }))
   }
 
   return (
     <div className="App">
       <Banner />
-      <Formulario times={times.map(time => time.nome)} aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
+      <Formulario 
+        times={times.map(time => time.nome)} 
+        aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}
+        
+        />
 
-      {times.map(time => <Time 
-        key={time.nome} 
-        nome={time.nome} 
-        corPrimaria={time.corPrimaria} 
-        corSecundaria={time.corSecundaria} 
+      {times.map((time,indice) => <Time 
+        mudarCor={mudarCorDoTime}
+        key={indice} 
+        time={time} 
         colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
+        aoDeletar={deletarColaborador}
       />)}   
 
     </div>
--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,12 +1,15 @@
+import { AiFillCloseCircle } from 'react-icons/ai'
 import './Colaborador.css'
-
-const Colaborador = ({ nome, imagem, cargo, corDeFundo }) => {
+const Colaborador = ({ colaborador, imagem, cargo, corDeFundo, deletar}) => {
     return (<div className='colaborador'>
         <div className='cabecalho' style={{ backgroundColor: corDeFundo }}>
-            <img src={imagem} alt={nome}/>
+            <img src={imagem} alt={colaborador}/>
+        </div>
+        <div className='deletar' >
+            <AiFillCloseCircle size={25} className="deletar" onClick={deletar}/>
         </div>
         <div className='rodape'>
-            <h4>{nome}</h4>
+            <h4>{colaborador}</h4>
             <h5>{cargo}</h5>
         </div>
     </div>)
